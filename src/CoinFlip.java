d8e3718666d3d344ad9eed4d1b1340ad597fb9fa
Brandon Scoppa <scoppa.brandon@gmail.com>
2019-04-13 23:50:58 -0400

    Player balance begins at $100. Bug fixes

--- src/CoinFlip.java
+++ src/CoinFlip.java
@@ -7,39 +7,46 @@ public class CoinFlip {
         //Variable initilization and instantiation
         String reflip = "";
         Scanner scan = new Scanner(System.in);
+        String guess = "";
 
-        //Initilize balance to 0
-        double balance = 0;
+        //Initilize balance to 100
+        double balance = 100;
+        System.out.println("Welcome to Coin Flip! $100 has been deposited into your account to get started!");
 
         //Creates coin
         Coin coin1 = new Coin();
 
         do {
             //Betting prompt
-            System.out.println("How much would you like to bet?");
+            System.out.println("How much would you like to bet? Current Balance: $" + balance);
             double bet = scan.nextDouble();
-
-            //Coin flip guess prompt
-            System.out.println("Heads or Tails?");
-            String guess = scan.next();
 
             //Coin flip
             coin1.flip();
 
-            //If users guess equals the coin
-            if (guess.equals(coin1.toString())) {
-                System.out.println(guess + "! You win: $" + bet); //User wins
-                balance += bet * 2; //Returns the users bet and adds winnings to their balance
+            //if user has enough money in their balance
+            if (bet <= balance) {
+                //Coin flip guess prompt
+                System.out.println("Heads or Tails?");
+                guess = scan.next();
+
+                if (guess.equals(coin1.toString())) { //If users guess equals the coin
+                    balance += bet * 2; //Returns the users bet and adds winnings to their balance
+                    System.out.println(guess + "! You win: $" + bet); //User wins
+                } else {
+                    System.out.println("You lose! Your balance has been reset to $100 to continue playing"); //User loses
+                    balance = 100; //Users balance is reset to 0 since they lost
+                }
             }
-            else {
-                System.out.println("You lose! Your balance has been reset"); //User loses
-                balance = 0; //Users balance is reset to 0 since they lost
-            }
+            else
+                System.out.println("You don't have enough money");
+
             System.out.println("Your balance: $" + balance); //The users balance is outputted
 
             //Prompts user to play again
             System.out.println("Play Again? Type Y for Yes, Type N for No");
             reflip = scan.next();
+
 
         } while(reflip.equals("Y"));
 
